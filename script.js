2fefaa1116d84af28220413d30898113deb1df16
Folkert <folkertstijnman@gmail.com>
2022-06-05 20:51:13 +0200

    website v1

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ function loopOver(data) {
         projectItem = data[i]
         const newDiv = document.createElement("div");
         const projectHeader = document.createElement("h3");
-        projectHeader.innerHTML = projectItem["name"];
-        newDiv.appendChild(projectHeader)
+        const projectImg = document.createElement("img");
         const newContent = document.createTextNode(projectItem["description"]);
+        projectImg.src = 'img/project_' + (i + 1) + '.png';
+        projectHeader.innerHTML = projectItem["name"];
+        newDiv.appendChild(projectImg);
+        newDiv.appendChild(projectHeader)
         newDiv.appendChild(newContent);
         const currentDiv = document.getElementById("project-content");
         currentDiv.appendChild(newDiv);
@@ -19,3 +22,13 @@ function loopOver(data) {
     }
     
 }
+
+$(document).ready(function() {
+    $('a[href^="#"]').click(function(event) {
+        var id = $(this).attr("href");    
+        var offset = 60;
+        var target = $(id).offset().top - offset;
+        $('html, body').animate({scrollTop:target}, 500);
+        event.preventDefault();
+    });
+    });
